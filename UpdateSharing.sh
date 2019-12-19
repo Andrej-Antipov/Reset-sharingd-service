9f54ced75101b96a29ebb279990148b702ded3a5
Andrew <36465597+Andrej-Antipov@users.noreply.github.com>
2019-12-19 19:23:25 +0300

    killall SIGTERM sharingd

---
+++ UpdateSharing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# FUNCS
+
+DISPLAY_NOTIFICATION(){
+~/Library/Application\ Support/UpdateSharing/terminal-notifier.app/Contents/MacOS/terminal-notifier -title "Update SharingD" -subtitle "${SUBTITLE}" -message "${MESSAGE}" 
+}
+
+MESSAGE_START(){
+        if [[ $loc = "ru" ]]; then
+        SUBTITLE="Сервис обновления sharingd запущен"
+        MESSAGE="Интервал обновления $timer секунд"
+        DISPLAY_NOTIFICATION &
+        else
+        SUBTITLE="SharingD Updating Service Started"
+        MESSAGE="Refresh interval is $timer seconds"
+        DISPLAY_NOTIFICATION &
+        fi
+}
+
+# INIT
+loc=`defaults read -g AppleLocale | cut -d "_" -f1`; if [[ ! $loc = "ru" ]]; then loc="en"; fi 
+if [[ -f ~/Library/Application\ Support/UpdateSharing/timer.txt ]]; then 
+    timer=$( cat ~/Library/Application\ Support/UpdateSharing/timer.txt )
+else
+    timer=10
+fi
+
+MESSAGE_START
+
+# MAIN
+    
+  while true
+do 
+
+killall SIGTERM sharingd; sleep $timer
+
+done
